51f68ed2469d2ea7e00579ac917470b7f7ebce24
Andrés Bonilla <abraham.go@outlook.com>
2024-03-21 21:34:11 -0600

    Feature: init configuration app

---
+++ Apps/TheMovieDB/TheMovieDB/TheMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDBApp.swift
+//  TheMovieDB
+//
+//  Created by Andrés Bonilla Gómez on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
