39c6a012008ccc57213f8820f78e9b328172db52
Andrés Bonilla <abraham.go@outlook.com>
2024-03-21 21:50:36 -0600

    Feature: api core framework added

---
+++ Data/Core/APICore/APICore/APICore.h
@@ -0,0 +1,18 @@
+//
+//  APICore.h
+//  APICore
+//
+//  Created by Andrés Bonilla Gómez on 21/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for APICore.
+FOUNDATION_EXPORT double APICoreVersionNumber;
+
+//! Project version string for APICore.
+FOUNDATION_EXPORT const unsigned char APICoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APICore/PublicHeader.h>
+
+
